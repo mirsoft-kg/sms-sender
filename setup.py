dc28b8334298cb5f4430278ddf1058022261cefd
Daniyar Chambylov <dan.chambylov@gmail.com>
2017-04-18 14:41:10 +0600

    fix utfs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sms_sender',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=['*.pyc']),
     include_package_data=True,
     zip_safe=False,
--- sms_sender/backends/__init__.py
+++ sms_sender/backends/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from sms_sender.backends.dummy import Dummy
 from sms_sender.backends.smsc import SMSC
 
