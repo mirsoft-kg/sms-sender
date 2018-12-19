e0db41e6701b92405c3f0754b11686a540ebb381
Sanzhar Kubanychbekov <sanjarkubanychbekov@gmail.com>
2018-12-19 16:07:36 +0600

    Add result for Nikita.Kg class as return value for send_sms method and add Nikita.Kg to init file

--- sms_sender/__init__.py
+++ sms_sender/__init__.py
@@ -6,6 +6,7 @@ from django.utils.module_loading import import_string
 def load_backend(path):
     return import_string(path)()
 
+
 def send_sms(*args, **kwargs):
     backend = load_backend(settings.SEND_SMS_BACKEND)
     return backend.send_sms(*args, **kwargs)
